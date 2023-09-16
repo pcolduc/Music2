47e25e5832dfcd4540d7fdbc12f49ab162a178ea
Owen <kimdosa1212@naver.com>
2023-09-16 20:26:22 +0900

    Initial commit

---
+++ src/main/java/com/example/music/controller/AppController.java
@@ -0,0 +1,25 @@
+package com.example.music.controller;
+
+
+import com.example.music.service.AppService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+
+//    @GetMapping("/search-music")
+    public ResponseEntity<String> searchMusic(@RequestParam("track") String trackName) {
+
+        try{
+            return AppService.search(trackName);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.ok().body("bad request");
+        }
+    }
+
+}
